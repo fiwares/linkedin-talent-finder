0d94a95e575bbbc3db56caebfaea79a985cd5e8d
Pierre FILSTROFF <pierre.filstroff@gmail.com>
2022-11-01 22:31:54 +0100

    Updated scripts + added profile example

--- scripts/python/example.py
+++ scripts/python/example.py
@@ -1,7 +1,9 @@
+import json
 from linkedin_api import Linkedin
 
 api = Linkedin('julian.bale67@gmail.com', '3@rFs92.ChuiG}Be07')
 
-people = api.search_people(keywords="ruby AND python AND java", limit=5)
-person = api.get_profile(public_id=people[0]['urn_id'])
-print(person)
+people = api.search_people(keywords="ruby AND python", limit=20, include_private_profiles=False, network_depths=["F", "S", "O"], offset=50)
+print(json.dumps(people))
+#person = api.get_profile(public_id=people[0]['urn_id'])
+#print(person)
